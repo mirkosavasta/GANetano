74f4ab7346aa6812c1db1375b4a2a944fe17c78f
Mirko Savasta <savastamirko@gmail.com>
2020-06-13 00:33:17 +0200

    Adds Pytorch dataset for sines

---
+++ datasets.py
@@ -0,0 +1,44 @@
+from typing import Sequence
+
+from torch.utils.data import Dataset
+import numpy as np
+
+
+class Sines(Dataset):
+
+    def __init__(self, frequency_range: Sequence[float, float], amplitude_range: Sequence[float, float],
+                 n_series: int = 200, datapoints: int = 400, seed: int = None):
+        """
+        Pytorch Dataset to produce sines
+
+        :param frequency_range:
+        :param amplitude_range:
+        :param n_series:
+        :param datapoints:
+        :param seed:
+        """
+        self.n_series = n_series
+        self.datapoints = datapoints
+        self.seed = seed
+        self.frequency_range = frequency_range
+        self.amplitude_range = amplitude_range
+        self.dataset = self._generate_sines()
+
+    def __len__(self):
+        return self.datapoints
+
+    def __getitem__(self, idx):
+        return self.dataset[idx, None]
+
+    def _generate_sines(self):
+        if self.seed is not None:
+            np.random.seed(self.seed)
+
+        x = np.linspace(start=0, stop=2 * np.pi, num=self.datapoints)
+        low_freq, up_freq = self.frequency_range[0], self.frequency_range[1]
+        low_amp, up_amp = self.amplitude_range[0], self.amplitude_range[1]
+
+        freq_vector = (up_freq - low_freq) * np.random.rand(self.n_series, 1) + low_freq
+        ampl_vector = (up_amp - low_amp) * np.random.rand(self.n_series, 1) + low_amp
+
+        return ampl_vector * np.sin(freq_vector * x)
