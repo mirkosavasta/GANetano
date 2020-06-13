0ec9bf9218ed24ff77a0a5cc1a550044c6dce49f
Mirko Savasta <savastamirko@gmail.com>
2020-06-13 12:27:50 +0200

    Adds Trainer

---
+++ train.py
@@ -0,0 +1,233 @@
+import argparse
+import os
+
+
+import torch
+from tqdm import tqdm
+from torch.autograd import Variable
+from torch.autograd import grad as torch_grad
+import matplotlib.pyplot as plt
+from torch.utils.tensorboard import writer, SummaryWriter
+from torch.utils.data import DataLoader
+from math import pi
+
+from datasets.datasets import Sines
+from models.wgangp import Generator, Critic
+
+
+class Trainer:
+    NOISE_LENGTH = 50
+
+    def __init__(self, generator, critic, gen_optimizer, critic_optimizer,
+                 gp_weight=10, critic_iterations=2, print_every=200, use_cuda=False, checkpoint_frequency=200):
+        self.g = generator
+        self.g_opt = gen_optimizer
+        self.c = critic
+        self.c_opt = critic_optimizer
+        self.losses = {'g': [], 'c': [], 'GP': [], 'gradient_norm': []}
+        self.num_steps = 0
+        self.use_cuda = use_cuda
+        self.gp_weight = gp_weight
+        self.critic_iterations = critic_iterations
+        self.print_every = print_every
+        self.checkpoint_frequency = checkpoint_frequency
+
+        if self.use_cuda:
+            self.g.cuda()
+            self.c.cuda()
+
+    def _critic_train_iteration(self, real_data):
+
+        batch_size = real_data.size()[0]
+        noise_shape = (batch_size, self.NOISE_LENGTH)
+        generated_data = self.sample_generator(noise_shape)
+
+        real_data = Variable(real_data)
+
+        if self.use_cuda:
+            real_data = real_data.cuda()
+
+        # Pass data through the Critic
+        c_real = self.c(real_data)
+        c_generated = self.c(generated_data)
+
+        # Get gradient penalty
+        gradient_penalty = self._gradient_penalty(real_data, generated_data)
+        self.losses['GP'].append(gradient_penalty.data.item())
+
+        # Create total loss and optimize
+        self.c_opt.zero_grad()
+        d_loss = c_generated.mean() - c_real.mean() + gradient_penalty
+        d_loss.backward()
+        self.c_opt.step()
+
+        self.losses['c'].append(d_loss.data.item())
+
+    def _generator_train_iteration(self, data):
+        self.g_opt.zero_grad()
+        batch_size = data.size()[0]
+        latent_shape = (batch_size, self.NOISE_LENGTH)
+
+        generated_data = self.sample_generator(latent_shape)
+
+        # Calculate loss and optimize
+        d_generated = self.c(generated_data)
+        g_loss = - d_generated.mean()
+        g_loss.backward()
+        self.g_opt.step()
+        self.losses['g'].append(g_loss.data.item())
+
+    def _gradient_penalty(self, real_data, generated_data):
+
+        batch_size = real_data.size()[0]
+
+        # Calculate interpolation
+        alpha = torch.rand(batch_size, 1)
+        alpha = alpha.expand_as(real_data)
+        if self.use_cuda:
+            alpha = alpha.cuda()
+        interpolated = alpha * real_data.data + (1 - alpha) * generated_data.data
+        interpolated = Variable(interpolated, requires_grad=True)
+        if self.use_cuda:
+            interpolated = interpolated.cuda()
+
+        # Pass interpolated data through Critic
+        prob_interpolated = self.c(interpolated)
+
+        # Calculate gradients of probabilities with respect to examples
+        gradients = torch_grad(outputs=prob_interpolated, inputs=interpolated,
+                               grad_outputs=torch.ones(prob_interpolated.size()).cuda() if self.use_cuda
+                               else torch.ones(prob_interpolated.size()), create_graph=True,
+                               retain_graph=True)[0]
+        # Gradients have shape (batch_size, num_channels, series length),
+        # here we flatten to take the norm per example for every batch
+        gradients = gradients.view(batch_size, -1)
+        self.losses['gradient_norm'].append(gradients.norm(2, dim=1).mean().data.item())
+
+        # Derivatives of the gradient close to 0 can cause problems because of the
+        # square root, so manually calculate norm and add epsilon
+        gradients_norm = torch.sqrt(torch.sum(gradients ** 2, dim=1) + 1e-12)
+
+        # Return gradient penalty
+        return self.gp_weight * ((gradients_norm - 1) ** 2).mean()
+
+    def _train_epoch(self, data_loader, epoch):
+        for i, data in enumerate(data_loader):
+            self.num_steps += 1
+            self._critic_train_iteration(data.float())
+            # Only update generator every critic_iterations iterations
+            if self.num_steps % self.critic_iterations == 0:
+                self._generator_train_iteration(data)
+
+            if i % self.print_every == 0:
+                global_step = i + epoch * len(data_loader.dataset)
+                writer.add_scalar('Losses/Critic', self.losses['c'][-1], global_step)
+                writer.add_scalar('Losses/Gradient Penalty', self.losses['GP'][-1], global_step)
+                writer.add_scalar('Gradient Norm', self.losses['gradient_norm'][-1], global_step)
+
+                if self.num_steps > self.critic_iterations:
+                    writer.add_scalar('Losses/Generator', self.losses['g'][-1], global_step)
+
+    def train(self, data_loader, epochs, plot_training_samples=True, checkpoint=None):
+
+        if checkpoint:
+            path = os.path.join('checkpoints', checkpoint)
+            state_dicts = torch.load(path, map_location=torch.device('cpu'))
+            self.g.load_state_dict(state_dicts['g_state_dict'])
+            self.c.load_state_dict(state_dicts['d_state_dict'])
+            self.g_opt.load_state_dict(state_dicts['g_opt_state_dict'])
+            self.c_opt.load_state_dict(state_dicts['d_opt_state_dict'])
+
+        if plot_training_samples:
+            # Fix latents to see how series generation improves during training
+            noise_shape = (1, self.NOISE_LENGTH)
+            fixed_latents = Variable(self.sample_latent(noise_shape))
+            if self.use_cuda:
+                fixed_latents = fixed_latents.cuda()
+
+        for epoch in tqdm(range(epochs)):
+
+            # Sample a different region of the latent distribution to check for mode collapse
+            dynamic_latents = Variable(self.sample_latent(noise_shape))
+            if self.use_cuda:
+                dynamic_latents = dynamic_latents.cuda()
+
+            self._train_epoch(data_loader, epoch + 1)
+
+            # Save checkpoint
+            if epoch % self.checkpoint_frequency == 0:
+                torch.save({
+                    'epoch': epoch,
+                    'd_state_dict': self.c.state_dict(),
+                    'g_state_dict': self.g.state_dict(),
+                    'd_opt_state_dict': self.c_opt.state_dict(),
+                    'g_opt_state_dict': self.g_opt.state_dict(),
+                }, 'checkpoints/epoch_{}.pkl'.format(epoch))
+
+            if plot_training_samples and (epoch % self.print_every == 0):
+                self.g.eval()
+                # Generate fake data using both fixed and dynamic latents
+                fake_data_fixed_latents = self.g(fixed_latents).cpu().data
+                fake_data_dynamic_latents = self.g(dynamic_latents).cpu().data
+
+                plt.figure()
+                plt.plot(fake_data_fixed_latents.numpy()[0].T)
+                plt.savefig('training_samples/fixed_latents/series_epoch_{}.png'.format(epoch))
+                plt.close()
+
+                plt.figure()
+                plt.plot(fake_data_dynamic_latents.numpy()[0].T)
+                plt.savefig('training_samples/dynamic_latents/series_epoch_{}.png'.format(epoch))
+                plt.close()
+                self.g.train()
+
+    def sample_generator(self, latent_shape):
+        latent_samples = Variable(self.sample_latent(latent_shape))
+        if self.use_cuda:
+            latent_samples = latent_samples.cuda()
+
+        return self.g(latent_samples)
+
+    @staticmethod
+    def sample_latent(shape):
+        return torch.randn(shape)
+
+    def sample(self, num_samples):
+        generated_data = self.sample_generator(num_samples)
+        return generated_data.data.cpu().numpy()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='GANetano', usage='%(prog)s [options]')
+    parser.add_argument('-ln', '--logname', type=str, dest='log_name', default=None, required=True,
+                        help='tensorboard filename')
+    parser.add_argument('-e', '--epochs', type=int, dest='epochs', default=15000, help='number of training epochs')
+    parser.add_argument('-bs', '--batches', type=int, dest='batches', default=16,
+                        help='number of batches per training iteration')
+    parser.add_argument('-cp', '--checkpoint', type=str, dest='checkpoint', default=None,
+                        help='checkpoint to use for a warm start')
+
+
+    args = parser.parse_args()
+
+    # Instantiate Generator and Critic + initialize weights
+    g = Generator()
+
+    g_opt = torch.optim.RMSprop(g.parameters(), lr=0.0005)
+    d = Critic()
+
+    d_opt = torch.optim.RMSprop(d.parameters(), lr=0.0005)
+
+    # Create Dataloader
+    dataset = Sines(frequency_range=[0, 2 * pi], amplitude_range=[0, 2 * pi], seed=42, n_series=200)
+    dataloader = DataLoader(dataset, batch_size=args.batches)
+
+    # Instantiate Trainer
+    trainer = Trainer(g, d, g_opt, d_opt, use_cuda=torch.cuda.is_available())
+    # Train model
+    print('Training is about to start...')
+    # Instantiate Tensorboard writer
+    tb_logdir = os.path.join('..', 'tensorboard', args.log_name)
+    writer = SummaryWriter(log_dir=tb_logdir)
+
+    trainer.train(dataloader, epochs=args.epochs, plot_training_samples=True, checkpoint=args.checkpoint)
