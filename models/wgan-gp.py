f3e63126b0b23b96640384f66960aacaf1b77a93
Mirko Savasta <savastamirko@gmail.com>
2020-06-13 10:50:40 +0200

    Adds G and D architectures

---
+++ models/wgan-gp.py
@@ -0,0 +1,78 @@
+from torch import nn
+from torch.nn.utils import spectral_norm
+
+
+class AddDimension(nn.Module):
+
+    def forward(self, x):
+        return x.unsqueeze(1)
+
+
+class SqueezeDimension(nn.Module):
+
+    def forward(self, x):
+        return x.squeeze(1)
+
+
+def create_generator_architecture():
+    return nn.Sequential(nn.Linear(50, 100),
+                         nn.LeakyReLU(0.2, inplace=True),
+                         AddDimension(),
+                         spectral_norm(nn.Conv1d(1, 32, 3, padding=1), n_power_iterations=10),
+                         nn.Upsample(200),
+
+                         spectral_norm(nn.Conv1d(32, 32, 3, padding=1), n_power_iterations=10),
+                         nn.LeakyReLU(0.2, inplace=True),
+                         nn.Upsample(400),
+
+                         spectral_norm(nn.Conv1d(32, 32, 3, padding=1), n_power_iterations=10),
+                         nn.LeakyReLU(0.2, inplace=True),
+                         nn.Upsample(800),
+
+                         spectral_norm(nn.Conv1d(32, 1, 3, padding=1), n_power_iterations=10),
+                         nn.LeakyReLU(0.2, inplace=True),
+
+                         SqueezeDimension(),
+                         nn.Linear(800, 100)
+                         )
+
+
+def create_critic_architecture():
+    return nn.Sequential(AddDimension(),
+                         spectral_norm(nn.Conv1d(1, 32, 3, padding=1), n_power_iterations=10),
+                         nn.LeakyReLU(0.2, inplace=True),
+                         nn.MaxPool1d(2),
+                         spectral_norm(nn.Conv1d(32, 32, 3, padding=1), n_power_iterations=10),
+                         nn.LeakyReLU(0.2, inplace=True),
+                         nn.MaxPool1d(2),
+
+                         spectral_norm(nn.Conv1d(32, 32, 3, padding=1), n_power_iterations=10),
+                         nn.LeakyReLU(0.2, inplace=True),
+                         nn.Flatten(),
+
+                         nn.Linear(800, 50),
+                         nn.LeakyReLU(0.2, inplace=True),
+
+                         nn.Linear(50, 15),
+                         nn.LeakyReLU(0.2, inplace=True),
+
+                         nn.Linear(15, 1)
+                         )
+
+
+class Generator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.main = create_generator_architecture()
+
+    def forward(self, input):
+        return self.main(input)
+
+
+class Critic(nn.Module):
+    def __init__(self):
+        super(Critic, self).__init__()
+        self.main = create_critic_architecture()
+
+    def forward(self, input):
+        return self.main(input)
